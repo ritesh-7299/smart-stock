54919d7f0b9cd5a79d48801a33db80be768c6da1
ritesh macwan <riteshmacwan07@gmail.com>
2023-08-06 20:35:23 +0530

    added: tailwind to project

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,9 +2,10 @@ import { Button, Header } from "ui";
 
 export default function Page() {
   return (
-    <>
+    <div className="content-center bg-red-500">
       <Header text="Web" />
+      <p className="text-center text-red-800">ritesh</p>
       <Button />
-    </>
+    </div>
   );
 }
---
+++ apps/web/app/stock/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <p className="text-red-900 text-3xl underline">page</p>
+    </div>
+  );
+}
